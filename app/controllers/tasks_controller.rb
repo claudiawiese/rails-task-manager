40e1712c7078e074a1dea99f79840ad723b8e089
Claudia Wiese <claudia.wiese@sciencespo.fr>
2019-02-12 16:16:43 +0100

    Process done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,20 +13,25 @@ class TasksController < ApplicationController
   end
 
   def create
-    task = Task.new(
-      task_params
-      )
+    task = Task.new(task_params)
     task.save
     redirect_to tasks_path
   end
 
   def update
+    task_id
+    @task.update(task_params)
+    redirect_to tasks_path
   end
 
   def edit
+    task_id
   end
 
   def destroy
+    task_id
+    @task.destroy
+    redirect_to tasks_path
   end
 
   private
